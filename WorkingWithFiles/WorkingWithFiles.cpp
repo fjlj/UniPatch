7ea8f56d01b9a520bca0cd1a5ea529ddac60ecdc
Give Academy LLC <info@give.academy>
2021-08-08 02:56:17 -0700

    create a backup by default...

--- WorkingWithFiles/WorkingWithFiles.cpp
+++ WorkingWithFiles/WorkingWithFiles.cpp
@@ -232,7 +232,7 @@ int main(int argc, char* argv[])
     //attempt to open in file
     if (!argv[1]) {
         cout << "Usage: UniPatch.exe <1337_file_path> [options]" << endl 
-                << "\t-b\tBackup Target" << endl
+                << "\t-nb\tDO NOT Backup Target" << endl
                 << "\t-r\tTreat addresses as file offsets" << endl
                 << "\t-f\tForce patch" << endl;
         return -1;
@@ -251,7 +251,7 @@ int main(int argc, char* argv[])
 
     cout << "Read " << std::dec << to_patch->patch_count << " patches." << endl << endl << "Beginning patching Process" << endl;
 
-    if (inArgs(argv, argc, "-b")) {
+    if (!inArgs(argv, argc, "-nb")) {
         CopyFileA(to_patch->PE_Name.c_str(),(to_patch->PE_Name + ".bak").c_str(), false);
     }
     target.open(to_patch->PE_Name, std::ios_base::binary | std::ios_base::out | std::ios_base::in);
